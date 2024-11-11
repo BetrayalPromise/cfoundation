103f7d06244b22e07899d687a149d8639db2ae09
MrYang <BetrayalPromise@gmail.com>
2024-11-11 18:24:36 +0800

    整理 宏工具

--- carray.h
+++ carray.h
@@ -20,23 +20,10 @@ typedef unsigned long  ulong;
      容量(0-...)  数量(0-...)  存储位置
 */
 
-typedef enum format {
-    original,   // 原码
-    inverse,    // 反码
-    complement, // 补码
-} format_t;
-
 typedef enum ISO_IEC_646 {
     C89,// ISO/IEC 646:1991 
     C99,// ISO/IEC 646:1999
 } ISO_IEC_646_t;
-
-#define BitGet(n, p)  ((n) >> (p)&1)        // 得到某数的某位
-#define BitNOT(n, p)  ((n) ^= 1<<(p))       // 把n的p位取反
-#define BitSET1(n, p) ((n)|= 1<<(p))        // 把某位置1
-#define BitSET0(n, p) ((n) &= ~(1<<(p)))    // 把某位置0
-
-#define EXCHANGE(n1, n2) do { n1 = n1 ^ n2; n2 = n1 ^ n2; n1 = n1 ^ n2; } while(0)
 
 typedef enum cbasetype {
     cbasechar   = 0x00UL,
@@ -100,16 +87,6 @@ extern bool         carrayinsert(void * ca, cainsert_t t, long idx, long ps, ...
 
 
 void endian();
-void char2binary(char value, format_t fmt);
-void short2binary(short value, format_t fmt);
-void int2binary(int value, format_t fmt);
-void long2binary(long value, format_t fmt);
-void uchar2binary(unsigned char value, format_t fmt);
-void ushort2binary(unsigned short value, format_t fmt);
-void uint2binary(unsigned int value, format_t fmt);
-void ulong2binary(unsigned long value, format_t fmt);
-void float2binary(float value, format_t fmt);
-void double2binary(double value, format_t fmt);
 void showcolor();
 
 #endif
--- cutils.h
+++ cutils.h
@@ -16,4 +16,125 @@
     #warning "information: duplicate define macro 'DEFAULTARGC'
 #endif
 
+// 针对动态链接库,用它定义的符号将被导出，动态库中的函数默认是可见的。
+#define public __attribute__((visibility("default")))
+// 针对动态链接库,用它定义的符号将不被导出，并且不能从其它对象进行使用，动态库中的函数是被隐藏的。
+#define private __attribute__((visibility("hidden")))
+
+#if !defined (__$NAMESPACE$__) && !defined (__NAMESPACE__)
+    #define __$NAMESPACE$__(s, Module, File, Name) Module##s##File##s##Name
+    #define __NAMESPACE__(s, Module, File, Name) __$NAMESPACE$__(s, Module, File, Name)
+#else
+    #warning "information: duplicate define macro '__$NAMESPACE$__', '__NAMESPACE__'!"
+#endif
+    
+#if !defined(func)
+    // Module: 模块名称
+    // File: 文件名称不带后缀
+    // Name: 函数名
+    // s:分割符号 C语言允许的函数名分隔符只有两种(_和$)其他的不允许使用
+    #define func(s, Module, File, Name) __NAMESPACE__(s, Module, File, Name)
+#else
+    #warning "information: duplicate define macro 'func'!"
+#endif
+
+#if !defined ($$) && !defined (__)
+    #define $$(Module, File, Name) func($, Module, File, Name)
+    #define __(Module, File, Name) func(_, Module, File, Name)
+#else
+    #warning "information: duplicate define macro '$$', '__'!"
+#endif
+
+
+#if !defined (init) && !defined (deinit)
+    #define init(type, n) ((type *)malloc((n) * sizeof(type)))
+    #define deinit(var) (free(var); var = NULL);
+#else
+    #warning "information: duplicate define macro 'init', 'deinit'!"
+#endif
+
+#if !defined (Stringfy)
+    #define Stringfy(x) #x
+#else
+    #warning "information: duplicate define macro 'Stringfy'!"
+#endif
+
+#if !defined(weak) && !defined (strong)
+    #define weak __attribute__((weak))
+    #define strong
+#else
+    #warning "information: duplicate define macro 'weak', 'strong'!"
+#endif
+
+#if !defined(FlexFor)
+    #define FlexFor(var, start, end, ...) for(int var = start; var <= end; __VA_ARGS__)
+#else
+    #warning "information: duplicate define macro 'EasyFor'!"
+#endif
+
+#if !defined(Minimum) && !defined (Maximum)
+    #define Minimum(type, x, y) ({ type __min1 = (x); type __min2 = (y); __min1 < __min2 ? __min1 : __min2; })
+    #define Maximum(type, x, y) ({ type __max1 = (x); type __max2 = (y); __max1 > __max2 ? __max1 : __max2; })
+#else
+    #warning "information: duplicate define macro 'Minimum', 'Maximum'!"
+#endif
+
+#if !defined(_LABEL_) && !defined(__LABEL__) && !defined(___LABEL___)
+    #define _LABEL_(x) _LABEL_##x
+    #define __LABEL__(x) _LABEL_(x)
+    #define ___LABEL___ __LABEL__(__LINE__)
+#else
+    #warning "information: duplicate define macro '_LABEL_', '__LABEL__', '___LABEL___'!"
+#endif
+
+#if !defined(DeferBegin) && !defined(defer) && !defined(DeferEnd)
+    #define DeferBegin(number)\
+        void * _defers_[number];\
+        void * _run_;\
+        int _index_ = 0;\
+        int _number_ = number;\
+
+    #define defer(code) \
+        if (_index_ > _number_ - 1) {\
+            printf("number too smaller!");\
+            abort();\
+        };\
+        _defers_[_index_ ++] = && ___LABEL___;\
+        if (0) {\
+            ___LABEL___:code;\
+            if (_index_ --) goto *_defers_[_index_];\
+            else goto *_run_;\
+        }
+
+    #define DeferEnd()\
+        _run_ = && ___LABEL___;\
+        if (_index_--)\
+            goto *_defers_[_index_];\
+        ___LABEL___:
+#else
+    #warning "information: duplicate define macro 'DeferBegin', 'defer', 'DeferEnd'!"
+#endif
+
+#if !defined(BitGet) && !defined(BitNOT) && !defined(BitSET1) && !defined(BitSET0)
+    #define BitGet(n, p)  ((n) >> (p)&1)        // 得到某数的某位
+    #define BitNOT(n, p)  ((n) ^= 1<<(p))       // 把n的p位取反
+    #define BitSET1(n, p) ((n)|= 1<<(p))        // 把某位置1
+    #define BitSET0(n, p) ((n) &= ~(1<<(p)))    // 把某位置0
+#else
+    #warning "information: duplicate define macro 'BitGet', 'BitNOT', 'BitSET0', 'BitSET1'!"
+#endif
+
+
+#if !defined(EXCHANGE)
+    #define EXCHANGE(n1, n2) do { n1 = n1 ^ n2; n2 = n1 ^ n2; n1 = n1 ^ n2; } while(0)
+#else
+    #warning "information: duplicate define macro 'EXCHANGE'!"
+#endif
+
+// static inline void handler(void (^ * expr)(void)) { (* expr)(); }
+// #define DEFER_1(x, y) x##y
+// #define DEFER_2(x, y) DEFER_1(x, y)
+// #define DEFER_0(x)    DEFER_2(x, __COUNTER__)
+// #define defer __attribute__((cleanup(handler))) void (^ DEFER_0(option))(void) = ^
+
 #endif
